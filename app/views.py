4629c88e2ebd23e8c0fb512e5dde7708f238ffed
sandeep-1829 <godugunurusandeep@gmail.com>
2024-01-31 17:34:59 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -23,3 +23,26 @@ def equijoins(request):
     EmpObjects=Emp.objects.select_related('deptno').filter(deptno__dlocation='DALLAS')
     d={'EmpObjects':EmpObjects}
     return render(request,'equijoins.html',d)
+
+
+
+
+def selfjoins(request):
+    EmpObjects=Emp.objects.select_related('mgr').all()
+    EmpObjects=Emp.objects.select_related('mgr').filter(sal__gte=2500)
+    EmpObjects=Emp.objects.select_related('mgr').filter(mgr__ename='KING')
+    EmpObjects=Emp.objects.select_related('mgr').filter(mgr__ename='SCOTT')
+    EmpObjects=Emp.objects.select_related('mgr').filter(mgr__ename='BLAKE')
+    EmpObjects=Emp.objects.select_related('mgr').filter(mgr__ename='KING',sal__lt=3000)
+    EmpObjects=Emp.objects.select_related('mgr').all()
+    EmpObjects=Emp.objects.select_related('mgr').filter(mgr__isnull=True)
+    EmpObjects=Emp.objects.select_related('mgr').filter(mgr__ename__isnull=True)
+    EmpObjects=Emp.objects.select_related('mgr').all()
+    EmpObjects=Emp.objects.select_related('mgr').filter(job='MANAGER',mgr__ename='KING')
+    EmpObjects=Emp.objects.select_related('mgr').filter(mgr__ename='JONES')
+    EmpObjects=Emp.objects.select_related('mgr').filter(mgr__ename='JONES',sal__lte=3000)
+    EmpObjects=Emp.objects.select_related('mgr').filter(job='SALESMAN')
+    EmpObjects=Emp.objects.select_related('mgr').filter(job='ANALYST')
+    EmpObjects=Emp.objects.select_related('mgr').all()
+    d={'EmpObjects':EmpObjects}
+    return render(request,'selfjoins.html',d)
--- project34/urls.py
+++ project34/urls.py
@@ -21,4 +21,5 @@ from app.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('equijoins/',equijoins,name='equijoins'),
+    path('selfjoins/',selfjoins,name='selfjoins'),
 ]
